39cb461a46816abd0523240c4296d2e86761e79c
asemjk <asemalsaiyadi@gmail.com>
2025-08-16 20:08:12 +0300

    save

--- intermediate/scheduler.py
+++ intermediate/scheduler.py
@@ -1,4 +1,4 @@
-import subprocess
+import subprocess #subprocess is used to run the batch file
 import time
 
 batch_file_path = 'git_push.bat'  # Replace with the actual path to your .bat file
@@ -7,7 +7,6 @@ interval_seconds = 120
 
 def run_batch_file():
     try:
-        # Run the batch file using subprocess
         result = subprocess.run(batch_file_path, shell=True, check=True, capture_output=True, text=True)
         print("Batch file executed successfully.")
         print("Output:", result.stdout)
