e892d1f375aba4dd70b973f8d3d11eb335404bf4
asemjk <asemalsaiyadi@gmail.com>
2025-08-22 10:58:07 +0300

    save

--- basics/operators.py
+++ basics/operators.py
@@ -1,3 +1,18 @@
+#unpacking
+datetime = (4,45,10)
+hour, minute, second = datetime
+print(hour)
+print(minute)
+print(second)
+
+hour,*others = datetime
+print(hour)
+print(others)
+
+'''
+
+
+print('===============')
 import fractions
 c = [2,3,4]
 
@@ -8,3 +23,5 @@ print(fractions.Fraction(1, 3) + fractions.Fraction(1, 6))  # Output: 1/2
 x = (2, 3, 4)
 y= (5, 6, 7)
 print(x @ y) # Output: 56
+
+'''
