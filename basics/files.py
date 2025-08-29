50ee246d8c0e9f15a55463715fea013c6c1af1e6
asemjk <asemalsaiyadi@gmail.com>
2025-08-29 19:58:07 +0300

    save

---
+++ basics/files.py
@@ -0,0 +1,17 @@
+import time
+import os
+
+filename = os.path.join(os.path.dirname(__file__), 'data.txt')
+#Read
+with open(filename,"r") as file:
+    data = file.read()
+    print(data)
+
+while True:
+    with open(filename,"wt") as file:
+        file.write(time.ctime() + "\n")
+        if(time.ctime().startswith("21")):
+            break
+        time.sleep(60)
+        file.flush()
+   
