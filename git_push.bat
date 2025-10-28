bf2b8ca71fb73b58d0508942717951074b7e4a8f
asemjk <asemalsaiyadi@gmail.com>
2025-10-28 20:18:28 +0300

    save

--- git_push.bat
+++ git_push.bat
@@ -3,10 +3,6 @@ echo Adding all changes...
 git config set advice.useCoreFSMonitorConfig false
 git config --global --add safe.directory .
 rem rm .git/index.lock
-git add .
-echo Committing changes...
-git commit -m "save"
-echo Pushing to remote repository...
-git push -f
+git saveme
 echo Done!
 exit
